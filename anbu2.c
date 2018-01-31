48f488d11e3ef6fce1415f1da09607767a5e2ee1
anbudhanush5 <35687834+anbudhanush5@users.noreply.github.com>
2018-01-31 14:53:57 +0530

    Update anbu2.c

--- anbu2.c
+++ anbu2.c
@@ -4,7 +4,7 @@ void main()
 {
     int num;
  
-    printf("Enter a number: \n");
+    printf("Enter a number \n");
     scanf("%d", &num);
     if (num > 0)
         printf("%d is a positive number \n", num);
